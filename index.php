7adcc4a1113fc997566e9299f78c3fcaa04323bb
Rullyarrfii <rully.jr45@gmail.com>
2025-09-21 02:36:51 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Hello World</h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ea perspiciatis delectus dicta possimus ullam sapiente fuga omnis molestias doloremque nam quidem minima et obcaecati, eligendi dolore error quisquam tempore laudantium!</p>
 </body>
 </html>
